b836782bfa3d87ca48e99cc61e37055dffaf90fb
JavohirDevloper <muhammadiyevj72@gmail.com>
2024-02-05 17:05:52 +0500

    user controller tugatildi

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,196 @@
+const Joi = require("joi");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// register user
+const registerAndLoginUser = async (req, res) => {
+  try {
+    const schema = Joi.object({
+      fullname: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().min(4).required(),
+    });
+
+    const { error } = schema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ error: error.details[0].message });
+    }
+
+    const { fullname, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ error: "Email already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({ fullname, email, password: hashedPassword });
+    const savedUser = await newUser.save();
+
+    const token = jwt.sign({ id: savedUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.status(201).json({ token, user: savedUser });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const registerUser = async (req, res) => {
+  try {
+    const schema = Joi.object({
+      fullname: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().min(4).required(),
+    });
+
+    const { error } = schema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ error: error.details[0].message });
+    }
+
+    await registerAndLoginUser(req, res);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+// login user
+const loginUser = async (req, res) => {
+  try {
+    const schema = Joi.object({
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    });
+
+    const { error } = schema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ error: error.details[0].message });
+    }
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.status(200).json({ token, user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// Foydalanuvchini yaratish
+const createUser = async (req, res) => {
+  try {
+    const schema = Joi.object({
+      fullname: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().min(4).required(),
+    });
+
+    const { error } = schema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ error: error.details[0].message });
+    }
+
+    const { email } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ error: "Bu email allaqachon ro'yxatdan o'tgan" });
+    }
+
+    const newUser = new User(req.body);
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// Foydalanuvchilarni o'qish
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// Foydalaniuvchilarni idisi bilan oʻqish
+const getUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// Foydalanuvchini yangilash
+const updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const schema = Joi.object({
+      fullname: Joi.string(),
+      email: Joi.string().email(),
+      password: Joi.string().min(4),
+      // Diğer alanlar ve doğrulama kuralları
+    });
+
+    const { error } = schema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ error: error.details[0].message });
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// Foydalanuvchini o'chirish
+const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await User.findByIdAndDelete(id);
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  createUser,
+  getUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+  registerUser,
+  loginUser,
+  registerAndLoginUser,
+};
