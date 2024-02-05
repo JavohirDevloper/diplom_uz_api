85e0122e22331bddc613abb96cdccc6b1562a84a
JavohirDevloper <muhammadiyevj72@gmail.com>
2024-02-05 21:17:41 +0500

    views models tugatildi

---
+++ src/models/Views.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const viewSchema = new mongoose.Schema({
+  id: {
+    type: mongoose.SchemaTypes.ObjectId,
+  },
+  // file_ref_id: {
+  //   type: mongoose.SchemaTypes.ObjectId,
+  //   ref: "File",
+  // },
+  user_ref_id: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "User",
+  },
+});
+
+const View = mongoose.model("View", viewSchema);
+
+module.exports = {
+  View,
+};
