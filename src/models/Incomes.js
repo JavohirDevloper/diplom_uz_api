7ba0c330bf7e0ed18397fd7c278f5b4969999fa0
JavohirDevloper <muhammadiyevj72@gmail.com>
2024-02-05 21:57:19 +0500

    incomes modles tugatildi

---
+++ src/models/Incomes.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const incomeSchema = new mongoose.Schema({
+  id: {
+    type: mongoose.SchemaTypes.ObjectId,
+  },
+  user_ref_id: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "User",
+  },
+  //   file_ref_id: {
+  // type: mongoose.SchemaTypes.ObjectId,
+  //     ref: "File"
+  //   },
+});
+
+const Income = mongoose.model("Income", incomeSchema);
+
+module.exports = Income;
