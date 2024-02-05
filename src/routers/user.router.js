64160f2610e6e2aeed081e59f2e830d8424c8a7b
JavohirDevloper <muhammadiyevj72@gmail.com>
2024-02-05 17:06:17 +0500

    user routes tugatildi

---
+++ src/routers/user.router.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const UserController = require("../controllers/user.controller");
+const isLoggedIn = require("../shared/auth/isLoggedIn");
+
+router.post("/user/register", UserController.registerAndLoginUser);
+router.post("/user/login", UserController.loginUser);
+// userlar uchun router
+router.post("/user", isLoggedIn, UserController.createUser);
+router.get("/user", isLoggedIn, UserController.getUsers);
+router.get("/user/:id", isLoggedIn, UserController.getUserById);
+router.put("/user/:id", isLoggedIn, UserController.updateUser);
+router.delete("/user/:id", isLoggedIn, UserController.deleteUser);
+
+module.exports = router;
