fe7e8dcd409045c98bcae226e025dd5ebc2f764f
JavohirDevloper <muhammadiyevj72@gmail.com>
2024-02-05 21:20:14 +0500

    index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const db = require("./db/db");
 const handleError = require("./shared/errors/handle");
-
-
+const UserRouter = require("./routers/user.router.js");
+const ViewsRouter = require("./routers/views.router.js");
 dotenv.config();
 const app = express();
 // app use
 app.use(express.json());
 app.use(cors());
+app.use(UserRouter);
+app.use(ViewsRouter)
 // db
 db();
 // error handle
