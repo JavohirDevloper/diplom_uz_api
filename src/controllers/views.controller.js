f66d819233f26a413da4bccb3116a2ef8976c92e
JavohirDevloper <muhammadiyevj72@gmail.com>
2024-02-06 20:07:35 +0500

    xatolar toʻgʻirlandi

--- src/controllers/views.controller.js
+++ src/controllers/views.controller.js
@@ -2,10 +2,11 @@ const { View } = require("../models/Views");
 
 const getViews = async (req, res) => {
   try {
-    const views = await View.find().populate("user_ref_id");
+    const views = await View.find().populate([
+      { path: "user_ref_id", params: "file_ref_id" },
+    ]);
     res.json(views);
   } catch (err) {
-    console.error("Viewsni o'qishda xatolik yuz berdi:", err);
     res.status(500).json({ error: "Serverda xatolik yuz berdi" });
   }
 };
@@ -14,13 +15,11 @@ const getViewsId = async (req, res) => {
   const { id } = req.params;
 
   try {
-    const view = await View.findById(id).populate("user_ref_id");
+    const view = await View.findById(id).populate([
+      { path: "user_ref_id", params: "file_ref_id" },
+    ]);
     res.json(view);
   } catch (err) {
-    console.error(
-      `ID bo'yicha viewni qidirishda xatolik yuz berdi: ${id}`,
-      err
-    );
     res.status(500).json({ error: "Serverda xatolik yuz berdi" });
   }
 };
@@ -32,23 +31,20 @@ const deleteViews = async (req, res) => {
     await View.findByIdAndDelete(id);
     res.json({ message: "View muvaffaqiyatli o'chirildi" });
   } catch (err) {
-    console.error(
-      `ID bo'yicha viewni o'chirishda xatolik yuz berdi: ${id}`,
-      err
-    );
     res.status(500).json({ error: "Serverda xatolik yuz berdi" });
   }
 };
 
 const createView = async (req, res) => {
-  const { user_ref_id } = req.body;
+  const { user_ref_id, file_ref_id } = req.body;
   try {
-    const view = new View({ user_ref_id });
+    const view = new View({ user_ref_id, file_ref_id });
     const savedView = await view.save();
-    const populatedView = await savedView.populate("user_ref_id");
+    const populatedView = await savedView.populate([
+      { path: "user_ref_id", params: "file_ref_id" },
+    ]);
     res.json(populatedView);
   } catch (err) {
-    console.error("View yaratishda xatolik yuz berdi:", err);
     res.status(500).json({ error: "Serverda xatolik yuz berdi" });
   }
 };
