f66578c0e057d33354866a460bbf4e8358b5f092
JavohirDevloper <muhammadiyevj72@gmail.com>
2024-02-06 14:23:17 +0500

    admin model tugatildi

---
+++ src/models/Admin.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const AdminSchema = new mongoose.Schema(
+  {
+    email: {
+      type: mongoose.SchemaTypes.String,
+      trim: true,
+      unique: true,
+      required: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    password: {
+      type: mongoose.SchemaTypes.String,
+      required: true,
+    },
+    fullname: {
+      type: mongoose.SchemaTypes.String,
+      required: true,
+    },
+    subscription_status: {
+      type: mongoose.SchemaTypes.String,
+    },
+    role: {
+      type: mongoose.SchemaTypes.String,
+      enum: ["user", "admin"],
+      default: "admin",
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
+
+const Admin = mongoose.model("Admin", AdminSchema);
+module.exports = Admin;
