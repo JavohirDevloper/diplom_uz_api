28b0f3786b0ca4fd485647026741aad03e78d083
JavohirDevloper <muhammadiyevj72@gmail.com>
2024-02-06 20:06:28 +0500

    xatolar toʻgʻirlandi

--- src/models/Views.js
+++ src/models/Views.js
@@ -4,10 +4,10 @@ const viewSchema = new mongoose.Schema({
   id: {
     type: mongoose.SchemaTypes.ObjectId,
   },
-  // file_ref_id: {
-  //   type: mongoose.SchemaTypes.ObjectId,
-  //   ref: "File",
-  // },
+  file_ref_id: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "File",
+  },
   user_ref_id: {
     type: mongoose.SchemaTypes.ObjectId,
     ref: "User",
