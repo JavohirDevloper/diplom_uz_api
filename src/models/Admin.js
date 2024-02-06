f5ab19076d9a620af6a01c0615dbbdb8d0d804c8
JavohirDevloper <muhammadiyevj72@gmail.com>
2024-02-06 15:14:55 +0500

    super_admin modelini tugatdim

--- src/models/Admin.js
+++ src/models/Admin.js
@@ -25,7 +25,7 @@ const AdminSchema = new mongoose.Schema(
     },
     role: {
       type: mongoose.SchemaTypes.String,
-      enum: ["user", "admin"],
+      enum: ["admin", "super_admin"],
       default: "admin",
     },
   },
