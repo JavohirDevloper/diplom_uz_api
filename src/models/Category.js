89f7ea0e2a0abba4e9edc9aeb1eeb557c5c1f85b
JavohirDevloper <muhammadiyevj72@gmail.com>
2024-02-06 16:52:04 +0500

    Category Modelini tuagtdim

---
+++ src/models/Category.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const CategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: mongoose.SchemaTypes.String,
+      required: [true, "Category name is required"],
+      unique: true,
+      trim: true,
+      minlength: [3, "Category name should be at least 3 characters long"],
+      maxlength: [50, "Category name should not exceed 50 characters"],
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
+
+const Category = mongoose.model("Category", CategorySchema);
+module.exports = Category;
