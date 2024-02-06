5cfe384b37f25fe1d050e1ff4f6cd092d2bc7641
JavohirDevloper <muhammadiyevj72@gmail.com>
2024-02-06 18:44:14 +0500

    filarni yuklash uchunn multer qilindi

--- src/shared/config/multer.config.js
+++ src/shared/config/multer.config.js
@@ -1,15 +1,17 @@
 const multer = require("multer");
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "public/");
+    cb(null, "upload/");
   },
-  filefullname: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalfullname);
+  filename: function (req, file, cb) {
+    const trimmedFileName = file.originalname.replace(/\s+/g, "").toLowerCase();
+    cb(null, Date.now() + "_" + trimmedFileName);
   },
 });
 
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith("image/")) {
+  if (file.mimetype) {
     cb(null, true);
   } else {
     cb(new Error("Only images are allowed!"), false);
