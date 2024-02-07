bca4ff34a4488843c6ec8cdf00c703ac9799a2b0
JavohirDevloper <muhammadiyevj72@gmail.com>
2024-02-07 22:42:51 +0500

    oder controller qoʻshildi

---
+++ src/controllers/order.controller.js
@@ -0,0 +1,114 @@
+const Joi = require("joi");
+const Order = require("../models/Order");
+
+const orderSchema = Joi.object({
+  order_name: Joi.string().required(),
+  salary: Joi.string().required(),
+  deadline: Joi.string().required(),
+  user_ref_id: Joi.string().required(),
+  cat_ref_id: Joi.string().required(),
+  sub_ref_id: Joi.string().required(),
+});
+
+const createOrder = async (req, res) => {
+  try {
+    const { error } = orderSchema.validate(req.body);
+    if (error) {
+      return res
+        .status(400)
+        .json({ success: false, error: error.details[0].message });
+    }
+
+    const order = new Order(req.body);
+    await order.save();
+    res.status(201).json({ success: true, data: order });
+  } catch (error) {
+    res.status(400).json({ success: false, error: error.message });
+  }
+};
+
+const getOrders = async (req, res) => {
+  try {
+    const orders = await Order.find().populate({
+      path: "user_ref_id",
+      path: "file_ref_id",
+      path: "sub_ref_id",
+    });
+    res.status(200).json({ success: true, data: orders });
+  } catch (error) {
+    res.status(400).json({ success: false, error: error.message });
+  }
+};
+
+const getOrderById = async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id)
+        .populate([
+          { path: "user_ref_id", path: "file_ref_id", path: "sub_ref_id" },
+        ]);
+    //   .populate([
+    //       { path: "user_ref_id" },
+    //     { path: "file_ref_id" },
+    //     { path: "sub_ref_id" },
+    //   ]);
+    if (!order) {
+      return res.status(404).json({ success: false, error: "Order not found" });
+    }
+    res.status(200).json({ success: true, data: order });
+  } catch (error) {
+    res.status(400).json({ success: false, error: error.message });
+  }
+};
+
+const updateOrder = async (req, res) => {
+  try {
+    const { error } = orderSchema.validate(req.body);
+    if (error) {
+      return res
+        .status(400)
+        .json({ success: false, error: error.details[0].message });
+    }
+
+    const order = await Order.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!order) {
+      return res.status(404).json({ success: false, error: "Order not found" });
+    }
+    res.status(200).json({ success: true, data: order });
+  } catch (error) {
+    res.status(400).json({ success: false, error: error.message });
+  }
+};
+
+const deleteOrderById = async (req, res) => {
+  try {
+    const order = await Order.findByIdAndDelete(req.params.id);
+    if (!order) {
+      return res.status(404).json({ success: false, error: "Order not found" });
+    }
+    res.status(200).json({ success: true, message: "Order deleted" });
+  } catch (error) {
+    res.status(400).json({ success: false, error: error.message });
+  }
+};
+
+const deleteOrder = async (req, res) => {
+    try {
+      const order = await Order.findByIdAndDelete(req.body);
+      if (!order) {
+        return res.status(404).json({ success: false, error: "Order not found" });
+      }
+      res.status(200).json({ success: true, message: "Order deleted" });
+    } catch (error) {
+      res.status(400).json({ success: false, error: error.message });
+    }
+  };
+module.exports = {
+  createOrder,
+  getOrders,
+  getOrderById,
+  updateOrder,
+  deleteOrderById,
+  deleteOrder,
+};
