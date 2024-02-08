a493b9a8e87f6de64dc3903df33ab3819de4d576
JavohirDevloper <muhammadiyevj72@gmail.com>
2024-02-08 14:26:27 +0500

    order routeri toʻgʻirlandi

--- src/routers/order.router.js
+++ src/routers/order.router.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const orderController = require("../controllers/order.controller");
+const isLoggedIn = require("../shared/auth/isLoggedIn");
 
 const router = express.Router();
 
-router.post("/order", orderController.createOrder);
-router.get("/order", orderController.getOrders);
-router.get("/order/:id", orderController.getOrderById);
-router.put("/order/:id", orderController.updateOrder);
-router.delete("/order/:id", orderController.deleteOrderById);
-router.delete("/order", orderController.deleteOrder);
+router.post("/order", isLoggedIn, orderController.createOrder);
+router.get("/order", isLoggedIn, orderController.getOrders);
+router.get("/order/:id", isLoggedIn, orderController.getOrderById);
+router.put("/order/:id", isLoggedIn, orderController.updateOrder);
+router.delete("/order/:id", isLoggedIn, orderController.deleteOrderById);
+router.delete("/order", isLoggedIn, orderController.deleteOrder);
 module.exports = router;
