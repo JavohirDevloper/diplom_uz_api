43bf9b8e62999e2ded8277e2d3e799af60cfcf0a
Javohir Muhammadiyev <138434351+JavohirDevloper@users.noreply.github.com>
2024-02-15 15:26:42 +0500

    Update file.router.js

--- src/routers/file.router.js
+++ src/routers/file.router.js
@@ -1,20 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const fileController = require("../controllers/file.controller.js");
+const {
+  createFile,
+  getFileByID,
+  updateFile,
+  deleteFile,
+} = require("../controllers/file.controller.js");
 const isLoggedIn = require("../shared/auth/isLoggedIn");
-const { upload } = require("../shared/config/multer.config");
-
 const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
   windowMs: 2 * 60 * 1000,
-  max: 10, 
-  message: "Foydalanuvchi hajmi limitga yetdi. Iltimos, keyinroq harakat qiling.",
+  max: 10,
+  message:
+    "Foydalanuvchi hajmi limitga yetdi. Iltimos, keyinroq harakat qiling.",
 });
 
-router.post("/files", isLoggedIn,limiter, upload.single("file"),fileController.createFile);
-router.get("/:id", isLoggedIn, limiter, fileController.getFileByID);
-router.put("/:id", isLoggedIn,limiter, fileController.updateFile);
-router.delete("/:id",isLoggedIn,limiter,  fileController.deleteFile);
+router.post("/videos", isLoggedIn, limiter, createFile);
+router.get("/videos/:id", isLoggedIn, limiter, getFileByID);
+router.put("/videos/:id", isLoggedIn, limiter, updateFile);
+router.delete("/videos/:id", isLoggedIn, limiter, deleteFile);
 
 module.exports = router;
